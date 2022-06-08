32f23252171791b2201f97c3e7e601dffb296fc5
jtbnz <57672422+jtbnz@users.noreply.github.com>
2022-06-09 11:13:14 +1200

    Update ili9341_init.h

--- components/ili9341/ili9341_init.h
+++ components/ili9341/ili9341_init.h
@@ -80,7 +80,7 @@ static const uint8_t PROGMEM INITCMD_TFT_ILI9481[] = {
   ILI9341_VSCRSADD, 1, 0x00,             // Vertical scroll zero
   ILI9341_PIXFMT  , 1, 0x55,
   ILI9341_FRMCTR1 , 2, 0x00, 0x18,
-  ILI9341_DFUNCTR , 3, 0x08, 0x82, 0x27, // Display Function Control
+  ILI9341_DFUNCTR , 3, 0x02, 0x02, 0x3B, // Display Function Control
   0xF2, 1, 0x00,                         // 3Gamma Function Disable
   ILI9341_GAMMASET , 1, 0x01,             // Gamma curve selected
   ILI9341_GMCTRP1 , 15, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, // Set Gamma
