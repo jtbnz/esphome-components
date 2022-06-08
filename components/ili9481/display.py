0ea0e064e49c1fe9fa582074d459b7b8cdbb101c
jtbnz <57672422+jtbnz@users.noreply.github.com>
2022-06-09 09:43:01 +1200

    Update display.py

--- components/ili9481/display.py
+++ components/ili9481/display.py
@@ -47,7 +47,7 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_DC_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_LED_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_COLOR_PALETTE, default="NONE"): COLOR_PALETTE,
+           # cv.Optional(CONF_COLOR_PALETTE, default="NONE"): COLOR_PALETTE,
             cv.GenerateID(CONF_RAW_DATA_ID): cv.declare_id(cg.uint8),
         }
     )
